43df2190a4a4d78cbf66e95d660f220abc83ad9e
Erik Ebsen <erik.ebsen@me.com>
2015-02-03 16:47:45 -0600

    Better message output and one unecessary file removed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,21 +9,22 @@ var ROCK       = 1
 
 // returns 1,2,3. called on div click event
 function userPicks (choice) {
-  if (choice === 'Rock') {
-    return 1;
-  }
-  if (choice === 'Paper') {
-    return 2;
-  }
-  if (choice === 'Scissors') {
-    return 3;
-  }
+  if (choice === 'Rock') return 1;
+  if (choice === 'Paper') return 2;
+  if (choice === 'Scissors') return 3;
   else return 0;
 }
 
 // returns 1,2,3 randomly. called on div click event
 function compPicks () {
   return Math.floor(Math.random() * 3);
+}
+
+function stringify (choice) {
+  if (choice === 1) return 'rock';
+  if (choice === 2) return 'paper';
+  if (choice === 3) return 'scissors';
+  else return '';
 }
 
 function gameTied (user, comp) {
@@ -57,7 +58,7 @@ function updateUser () {
   //   (didYouWin()) ? 'You won!' : 'You lost';
   // );
 
-  $('#result').text('You picked ' + userChoice + '. The computer picked ' + compChoice + '. ' + gameResult );
+  $('#result').text('You picked ' + stringify( userChoice ) + '. The computer picked ' + stringify( compChoice ) + '. ' + gameResult );
   // if (gameTied) $('#result').appendText( 'You tied.' );
 }
 

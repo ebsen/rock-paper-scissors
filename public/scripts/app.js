52098a7187348a8673a53f80c0a34ddb63d8287e
Erik Ebsen <erik.ebsen@me.com>
2015-05-27 14:13:34 -0500

    Moved compare function inside Model and improved the UI output letting the player know who won

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,9 +13,39 @@ var Model = {
     // };
     return Model[choice.toUpperCase()];
   },
+  compare (player, computer) {
+    // return 1 if player won
+    // return -1 if computer won
+    // return 0 if tie
+    console.log(player);
+    console.log(computer);
+
+    if (player.id === computer.id) {
+      return 0;
+    } else {
+      // Scissors beats paper.
+      // Paper beats rock.
+      // Rock beats scissors.
+      if (player === Model.ROCK) {
+        if (computer === Model.SCISSORS) return 1;
+        else return -1;
+      }
+      if (player === Model.PAPER) {
+        if (computer === Model.ROCK) return 1;
+        else return -1;
+      }
+      if (player === Model.SCISSORS) {
+        if (computer === Model.PAPER) return 1;
+        else return -1;
+      }
+      // otherwise you lost
+      else return -1;
+    }
+  },
   random: function () {
     // return Math.floor(Math.random() * 3 + 1);
     var selection = Math.floor(Math.random() * 3 + 1);
+
     for (option in Model) {
       if (Model.hasOwnProperty(option)) {
         var property = Model[option.toUpperCase()];
@@ -31,48 +61,20 @@ var Model = {
   }
 };
 
-function compare (player, computer) {
-  // return 1 if player won
-  // return -1 if computer won
-  // return 0 if tie
-  console.log(player);
-  console.log(computer);
-  if (player.id === computer.id) {
-    return 0;
-  }
-  else {
-    // Scissors beats paper.
-    // Paper beats rock.
-    // Rock beats scissors.
-    if (player === Model.ROCK) {
-      if (computer === Model.SCISSORS) {
-        return 1;
-      }
-    }
-    if (player === Model.PAPER) {
-      if (computer === Model.ROCK) {
-        return 1;
-      }
-    }
-    if (player === Model.SCISSORS) {
-      if (computer === Model.PAPER) {
-        return 1;
-      }
-    }
-    // otherwise you lost
-    else return -1;
-  }
-}
-
-// Basically our `function main`
+// Basically our `function main()`
 $('#game').on('click', '.selection', function (event) {
   event.preventDefault(); // whatever that does
 
+  // Set up the game
   var playerOne = Model.choose($(this).text());
   var playerTwo = Model.random();
-  var winner = compare(playerOne, playerTwo);
-  if (winner === 0) {
-    $('#result').text('You both picked ' + playerOne.choice + '. Tie game.');
-  } else $('#result').text('You picked ' + playerOne.choice + '. The computer picked ' + playerTwo.choice + '. You ' + ((winner === 1) ? 'won' : 'lost') + '.');
+  var winner    = Model.compare(playerOne, playerTwo);
+  console.log(winner);
 
+  // Update the page with the result
+  if (winner === 0) {
+    $('#result').html('<p>You both picked <em>' + playerOne.choice + '</em>.</p><p><strong>Tie game.</strong></p>');
+  } else {
+    $('#result').html('<p>You picked <em>' + playerOne.choice + '</em>.<br />The computer picked <em>' + playerTwo.choice + '</em>.</p><p><strong>You ' + ((winner === 1) ? 'won' : 'lost') + '!</strong></p>');
+  }
 });
